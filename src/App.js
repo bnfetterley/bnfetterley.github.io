5686f221c3b46a05332ed0ff0c4812ff66d8cdd8
Bri Fetterley <bnfetterley@usfca.edu>
2020-06-18 17:58:01 -0400

    updated landing page

--- src/App.js
+++ src/App.js
@@ -6,7 +6,15 @@ import Nav from './Nav';
 function App() {
   return (
     <div>
-      <Nav />
+      <article className="vh-100 dt w-100 bg-green o-40">
+        <div className="dtc v-mid tc white ph3 ph4-l">
+          <h1 className="f6 f2-m f-subheadline-l fw6 tc">
+            Hi! I'm Bri, a full stack web developer.
+            <img></img>
+            <Nav />
+          </h1>
+        </div>
+      </article>
     </div>
   );
 }
--- src/Nav.js
+++ src/Nav.js
@@ -5,32 +5,40 @@ export default class Nav extends Component {
     return (
       <div>
         <nav className="pa3 pa4-ns">
-          <a
+          {/* <a
             className="link dim black b f1 f-headline-ns tc db mb3 mb4-ns"
             href="#"
             title="Home"
           >
-            Bri Fetterley
-          </a>
+        
+          </a> */}
           <div className="tc pb3">
-            <a className="link dim gray f6 f5-ns dib mr3" href="#" title="Home">
+            <a
+              className="link dark-gray f10 fw7 f5-ns dib mr3 hover-orange"
+              href="#"
+              title="Home"
+            >
               Home
             </a>
             <a
-              className="link dim gray f6 f5-ns dib mr3"
+              className="link dark-gray f6 fw7 f5-ns dib mr3 hover-orange"
               href="#"
               title="About"
             >
               Portfolio
             </a>
             <a
-              className="link dim gray f6 f5-ns dib mr3"
+              className="link dark-gray f6 fw7 f5-ns dib mr3 hover-orange"
               href="#"
               title="Store"
             >
               CV
             </a>
-            <a className="link dim gray f6 f5-ns dib" href="#" title="Contact">
+            <a
+              className="link dark-gray f6 fw7 f5-ns dib hover-orange"
+              href="#"
+              title="Contact"
+            >
               Contact
             </a>
           </div>
