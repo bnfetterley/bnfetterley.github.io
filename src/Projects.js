e27946411b68808f7bcde81e2fc177ccd86aef8c
Bri Fetterley <bnfetterley@usfca.edu>
2020-07-12 17:42:03 -0400

    updated projects page

--- src/Projects.js
+++ src/Projects.js
@@ -4,6 +4,11 @@ import traveltongue from './images/traveltongue.jpg';
 import artcollect from './images/artcollect.jpg';
 
 export default class Projects extends Component {
+  onclicklinks(e) {
+    e.preventDefault();
+    window.open('https://traveltongue-backend.herokuapp.com/');
+    window.open('https://traveltongue.netlify.app');
+  }
   render() {
     return (
       <div>
@@ -11,51 +16,70 @@ export default class Projects extends Component {
         <section class="mw7 center avenir">
           <h2 class="baskerville fw1 ph3 ph0-l">Projects</h2>
           <article class="bt bb b--black-10">
-            <a
+            {/* <a
               class="db pv4 ph3 ph0-l no-underline black dim"
               target="_blank"
-              href="https://traveltongue.netlify.app"
-            >
-              <div class="flex flex-column flex-row-ns">
-                <div class="pr3-ns mb4 mb0-ns w-100 w-40-ns">
-                  <img
-                    src={traveltongue}
-                    class="db"
-                    alt="Snapshot photo of a webpage"
-                  />
-                </div>
-                <div class="w-100 w-60-ns pl3-ns">
-                  <h1 class="f3 fw1 baskerville mt0 lh-title">TravelTongue</h1>
-                  <p class="f6 fw5 f5-l lh-copy">
-                    TravelTongue is a web app that allows users to discover
-                    local dishes from around the world.{' '}
-                  </p>
-                  <p>
-                    Developed a Rails API with RESTful endpoints for users,
-                    dishes and locations
-                  </p>
-                  <p class="f6 f5-l lh-copy">
-                    Utilized JSON Web Tokens and local storage to store
-                    encrypted user information traveltongue.jpgclient-side and
-                    bcrypt server-side{' '}
-                  </p>
-                  <p class="f6 f5-l lh-copy">
-                    Implemented GoogleMaps and World Bank API to render a
-                    dynamic world map{' '}
-                  </p>
-                  <p class="f6 f5-l lh-copy">
-                    Created pop up modals using pure CSS which allow users to
-                    add new dishes seamlessly
-                  </p>
-                  <p class="f6 f5-l lh-copy">
-                    Managed state, props and store in React and Redux for
-                    scalability
-                  </p>
-
-                  {/* <p class="f6 lh-copy mv0">By Robin Darnell</p> */}
-                </div>
+              // href="https://traveltongue.netlify.app"
+              // href="https://traveltongue.netlify.app"
+              onClick={(e) => this.onclicklinks}
+              // onclick="window.open('https://traveltongue-backend.herokuapp.com/');window.open('https://traveltongue.netlify.app')"
+            > */}
+            <div class="flex flex-column flex-row-ns">
+              <div class="pr3-ns mb4 mb0-ns w-100 w-40-ns">
+                <p></p>
+                <img
+                  src={traveltongue}
+                  class="db"
+                  alt="Snapshot photo of a webpage"
+                />
               </div>
-            </a>
+              <div class="w-100 w-60-ns pl3-ns">
+                <p></p>
+                <h1 class="f3 fw1 baskerville mt0 lh-title">TravelTongue</h1>
+                <p class="f6 fw5 f5-l lh-copy">
+                  TravelTongue is a web app that allows users to discover local
+                  dishes from around the world.{' '}
+                </p>
+                <p>
+                  Developed a Rails API with RESTful endpoints for users, dishes
+                  and locations
+                </p>
+                <p class="f6 f5-l lh-copy">
+                  Utilized JSON Web Tokens and local storage to store encrypted
+                  user information traveltongue.jpgclient-side and bcrypt
+                  server-side{' '}
+                </p>
+                <p class="f6 f5-l lh-copy">
+                  Implemented GoogleMaps and World Bank API to render a dynamic
+                  world map{' '}
+                </p>
+                <p class="f6 f5-l lh-copy">
+                  Created pop up modals using pure CSS which allow users to add
+                  new dishes seamlessly
+                </p>
+                <p class="f6 f5-l lh-copy">
+                  Managed state, props and store in React and Redux for
+                  scalability
+                </p>
+                <p class="f6 fw5 f5-l lh-copy">
+                  See it live by clicking below:{' '}
+                </p>
+                <p>
+                  <a target="_blank" href="https://traveltongue.netlify.app">
+                    frontend
+                  </a>{' '}
+                  <a
+                    target="_blank"
+                    href="https://traveltongue-backend.herokuapp.com/"
+                  >
+                    backend
+                  </a>
+                </p>
+
+                {/* <p class="f6 lh-copy mv0">By Robin Darnell</p> */}
+              </div>
+            </div>
+            {/* </a> */}
           </article>
           <article class="bb b--black-10">
             <a class="db pv4 ph3 ph0-l no-underline black dim" href="#0">
