4ec42db15aba5491e531d4392d9c2ac6e07420a8
Bri Fetterley <bnfetterley@usfca.edu>
2020-08-03 20:27:43 -0400

    Updates

---
+++ precache-manifest.12a2911c79dc9527fb97d02c2cc9fafe.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92b85cd4f81eb419e781c53990b02f33",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b4a0a172ef3be1c94bf2",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "937b5111abd99b1fa70e",
+    "url": "/static/js/2.a2a460e6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a2a460e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4a0a172ef3be1c94bf2",
+    "url": "/static/js/main.55b22871.chunk.js"
+  },
+  {
+    "revision": "3b2bda409d83d3f997fa",
+    "url": "/static/js/runtime-main.2fc13a78.js"
+  },
+  {
+    "revision": "f0b67fdb281e5a507f83043ddf4acc8c",
+    "url": "/static/media/artcollect.064de741.jpg"
+  },
+  {
+    "revision": "195f00863d9f27db8c5140c08f40c5cd",
+    "url": "/static/media/headshot.3371c442.png"
+  },
+  {
+    "revision": "8796e7ef5fcde97a34bad7cfd4fe9ffd",
+    "url": "/static/media/resume.cf1ac951.png"
+  },
+  {
+    "revision": "e89906264d5774453afc3031f0aaf0ee",
+    "url": "/static/media/traveltongue.cc03ff92.jpg"
+  }
+]);
